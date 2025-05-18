d97388d8d49b82b24f37b6643eeb9044cf52878a
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-05-18 23:57:58 +0200

    Controller done. Started the receiver.

---
+++ Core/Domain/Response/BusResponse.cs
@@ -0,0 +1,75 @@
+﻿namespace Core.Domain.Response;
+
+public class BusResponse : BaseResponse
+{
+    // TRUE
+    public static BusResponse Ok()
+    {
+        return new BusResponse { Success = true, StatusCode = 200 };
+    }
+    public static BusResponse Created()
+    {
+        return new BusResponse { Success = true, StatusCode = 201 };
+    }
+
+    // FALSE
+    public static BusResponse BadRequest(string? message)
+    {
+        return new BusResponse { Success = false, StatusCode = 400, Message = message };
+    }
+    public static BusResponse Unauthorized(string? message)
+    {
+        return new BusResponse { Success = false, StatusCode = 401, Message = message };
+    }
+    public static BusResponse Forbidden(string? message)
+    {
+        return new BusResponse { Success = false, StatusCode = 403, Message = message };
+    }
+    public static BusResponse NotFound(string? message)
+    {
+        return new BusResponse { Success = false, StatusCode = 404, Message = message };
+    }
+    public static BusResponse AlreadyExists(string? message)
+    {
+        return new BusResponse { Success = false, StatusCode = 409, Message = message };
+    }
+    public static BusResponse Error(string? message)
+    {
+        return new BusResponse { Success = false, StatusCode = 500, Message = message };
+    }
+    public static BusResponse BadGateway(string? message)
+    {
+        return new BusResponse { Success = false, StatusCode = 502, Message = message };
+    }
+    public static BusResponse ServiceUnavailable(string? message)
+    {
+        return new BusResponse { Success = false, StatusCode = 504, Message = message };
+    }
+
+}
+public class BusResponse<T> : BaseResponse<T> where T : class
+{
+    // TRUE
+    public static BusResponse<T> Ok(T? content)
+    {
+        return new BusResponse<T> { Success = true, StatusCode = 200, Content = content };
+    }
+
+    // FALSE
+    public static BusResponse<T> BadRequest(string? message, T? content)
+    {
+        return new BusResponse<T> { Success = false, StatusCode = 400, Message = message, Content = content };
+    }
+    public static BusResponse<T> NotFound(string? message, T? content)
+    {
+        return new BusResponse<T> { Success = false, StatusCode = 404, Message = message, Content = content };
+    }
+    public static BusResponse<T> AlreadyExists(string? message, T? content)
+    {
+        return new BusResponse<T> { Success = false, StatusCode = 409, Message = message, Content = content };
+    }
+    public static BusResponse<T> Error(string? message, T? content)
+    {
+        return new BusResponse<T> { Success = false, StatusCode = 500, Message = message, Content = content };
+    }
+}
--- Presentation/Controllers/TicketsController.cs
+++ Presentation/Controllers/TicketsController.cs
@@ -1,25 +1,51 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Core.Domain.Models;
+using Core.Factories;
+using Core.Interfaces;
+using Infrastructure.Repository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Presentation.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class TicketsController : ControllerBase
+public class TicketsController(ITicketService ticketService) : ControllerBase
 {
+    private readonly ITicketService _ticketService = ticketService;
 
-    //[HttpPost]
-    //public Task<IActionResult> Create()
-    //{
+    // Getting all the tickets that the user has for all events.
+    [HttpGet("user/{userId}")]
+    public async Task<IActionResult> GetAllUsersTickets(string userId)
+    {
+        var tickets = await _ticketService.GetAllUsersTicketsAsync(userId);
+        if (tickets == null) { return NotFound("Tickets not found."); }
+        
+        return Ok(tickets);
+    }
 
+    // Getting all the tickets of the user from that event.
+    [HttpGet("user/{userId}/event/{eventId}")]
+    public async Task<IActionResult> GetAllUsersTicketsAtEvent(string userId, string eventId)
+    {
+        var ueKey = RequestFactory.Create(userId, eventId);
+        if (ueKey == null) { return BadRequest("Invalid information given to the factory."); }
 
+        var tickets = await _ticketService.GetAllUsersTicketsAtEventAsync(ueKey);
+        if (tickets == null) { return NotFound("Tickets not found."); }
 
+        return Ok(tickets);
+    }
 
+    // Getting one single ticket for a user at one event.
+    [HttpGet("user/{userId}/event/{eventId}/seat/{seatNumber}")]
+    public async Task<IActionResult> GetATicket(string userId, string eventId, string seatNumber)
+    {
+        var uesKey = RequestFactory.Create(userId, eventId, seatNumber);
+        if (uesKey == null) { return BadRequest("Invalid information given to the factory."); }
 
+        var ticket = await _ticketService.GetTicketAsync(uesKey);
+        if (ticket == null) { return NotFound("Ticket not found."); }
 
-    //}
-
-
-
-
+        return Ok(ticket);
+    }
 }
