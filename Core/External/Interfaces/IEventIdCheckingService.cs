1cf1edc1238217dce48c48771642a1173e1c5934
Simon <152021717+SimonR-prog@users.noreply.github.com>
2025-05-21 21:09:10 +0200

    Service bus receiver and whatever else is done for now. External services need to be fixed more in the future after i learn grpc and whatever else is needed..

---
+++ Core/External/Interfaces/IEventIdCheckingService.cs
@@ -0,0 +1,9 @@
+﻿using Core.External.Models.Response;
+
+namespace Core.External.Interfaces
+{
+    public interface IEventIdCheckingService
+    {
+        Task<ExternalResponse> EventExistanceCheck(string eventId);
+    }
+}
---
+++ Core/External/Models/Options/BaseOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace Core.External.Models.Options;
+
+public class BaseOptions
+{
+    public string Url { get; set; } = null!;
+}
